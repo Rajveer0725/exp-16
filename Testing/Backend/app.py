4cb0ca87a10e682b0f6edfa1ceb9e3a2899738ee
Rajveer Arora <rajveerarora0725@gmail.com>
2026-04-17 12:00:48 +0530

    Added GitHub Actions workflow

--- Testing/Backend/app.py
+++ Testing/Backend/app.py
@@ -2,11 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Flask API is running 🚀"
+
 @app.route("/add")
 def add():
-    a = int(request.args.get("a"))
-    b = int(request.args.get("b"))
-    return jsonify({"result": a + b})
+    try:
+        a = int(request.args.get("a"))
+        b = int(request.args.get("b"))
+        return jsonify({"result": a + b})
+    except (TypeError, ValueError):
+        return jsonify({"error": "Please provide valid integers a and b"}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
